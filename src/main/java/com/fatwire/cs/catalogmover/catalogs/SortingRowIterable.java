b4b965a0bb7d5cda7ddb7903153d34f8aa00cc80
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2007-07-02 12:37:00 +0000

    Adding constructor to add a comprator for the sort strategy

--- src/main/java/com/fatwire/cs/catalogmover/catalogs/SortingRowIterable.java
+++ src/main/java/com/fatwire/cs/catalogmover/catalogs/SortingRowIterable.java
@@ -1,15 +1,24 @@
 package com.fatwire.cs.catalogmover.catalogs;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class SortingRowIterable implements Iterable<Row> {
 
-    final Set<Row> sorted = new TreeSet<Row>(new RowComparator());
+    private final Set<Row> sorted;
 
     public SortingRowIterable(final Iterable<Row> delegate) {
+        this(delegate, new RowComparator());
+        for (Row row : delegate) {
+            sorted.add(row);
+        }
+    }
 
+    public SortingRowIterable(final Iterable<Row> delegate,
+            Comparator<Row> comparator) {
+        sorted = new TreeSet<Row>(comparator);
         for (Row row : delegate) {
             sorted.add(row);
         }
